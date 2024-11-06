1819c5d499cfc7c6cf1b0034b3e41a58ff3f6ee4
hotcat190 <nguyendinhnguyen151204@gmail.com>
2024-11-06 09:38:32 +0700

    init

---
+++ index.js
@@ -0,0 +1,36 @@
+import dotenv from "dotenv";
+dotenv.config();
+
+import { Client, GatewayIntentBits } from ('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.MessageContent, GatewayIntentBits.GuildMessages] });
+
+const token = 'YOUR_BOT_TOKEN';
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('messageCreate', message => {
+    if (message.content.toLowerCase().startsWith('!guess')) {
+        const args = message.content.split(' ');
+        const userGuess = args[1];
+        
+        if (userGuess !== 'en' && userGuess !== 'jp') {
+            message.reply('Please guess either "en" or "jp"!');
+            return;
+        }
+
+        const outcomes = ['en', 'jp'];
+        const botChoice = outcomes[Math.floor(Math.random() * outcomes.length)];
+
+        if (userGuess === botChoice) {
+            message.reply(`Correct! It was "${botChoice}". 🎉`);
+        } else {
+            message.reply(`Sorry, it was actually "${botChoice}". Better luck next time!`);
+        }
+    }
+});
+
+client.login(token);
+
+
